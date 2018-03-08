d1dd6e4ee7bef65b03ee210f580ac23b63e9225f
TheRingMaster <gzroms@hotmail.com>
2018-03-08 23:35:29 +0530

    Settings: Add toggle for proximity check on wake [2/2]
    kubersharma001 edits: fix settings search FC
    
    import com.android.settings.search.BaseSearchIndexProvider;
    import com.android.settings.search.Indexable;
    
    ganeshi4u edits: adapt to androidx (Q)
    
    [ghostrider-reborn: switch to TogglePreferenceController]
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I3c3875666340555cfaa7fb2f4a4c028c5b5ea2a2

---
+++ src/com/android/settings/security/ProximityOnWakePreferenceController.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.android.settings.security;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+
+import static android.provider.Settings.System.PROXIMITY_ON_WAKE;
+
+public class ProximityOnWakePreferenceController extends TogglePreferenceController {
+
+    public ProximityOnWakePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_proximityCheckOnWake)
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return Settings.System.getInt(mContext.getContentResolver(), PROXIMITY_ON_WAKE, 0) == 1;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        return Settings.System.putInt(mContext.getContentResolver(), PROXIMITY_ON_WAKE, isChecked ? 1 : 0);
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_security;
+    }
+}
