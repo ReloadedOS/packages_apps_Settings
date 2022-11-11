1eb1ac8b527efbce0a3eb4368e3b89ef0690a366
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-11-12 03:40:04 +0530

    Settings: Create a new fragment for ReloadedOS version

--- src/com/android/settings/deviceinfo/ReloadedVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/ReloadedVersionPreferenceController.java
@@ -1,22 +1,16 @@
 /*
  * Copyright (C) 2022 ReloadedOS
- *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.android.settings.deviceinfo;
 
 import android.content.Context;
-import android.os.SystemProperties;
 
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 
 public class ReloadedVersionPreferenceController extends BasePreferenceController {
-
-    private static final String RELOADED_VERSION_PROP = "ro.reloaded.version";
-    private static final String RELOADED_BUILDTYPE_PROP = "ro.reloaded.build_type";
-    private static final String RELOADED_VARIANT_PROP = "ro.reloaded.build_variant";
 
     private final Context mContext;
 
@@ -27,19 +21,18 @@ public class ReloadedVersionPreferenceController extends BasePreferenceControlle
 
     @Override
     public int getAvailabilityStatus() {
-        return AVAILABLE_UNSEARCHABLE;
+        return AVAILABLE;
     }
 
     @Override
     public CharSequence getSummary() {
-        String version = SystemProperties.get(RELOADED_VERSION_PROP,
-                mContext.getString(R.string.device_info_default));
-        String buildType = SystemProperties.get(RELOADED_BUILDTYPE_PROP,
-                mContext.getString(R.string.device_info_default));
-        String variant = SystemProperties.get(RELOADED_VARIANT_PROP,
-                mContext.getString(R.string.device_info_default));
-
         // Example: 13.0 | OFFICIAL | GAPPS
-        return version + " | " + buildType + " | " + variant;
+        StringBuilder sb = new StringBuilder(ReloadedVersionSettings.getVersion(mContext))
+                .append(" | ")
+                .append(ReloadedVersionSettings.getBuildType(mContext))
+                .append(" | ")
+                .append(ReloadedVersionSettings.getBuildVariant(mContext));
+
+        return sb.toString();
     }
 }
