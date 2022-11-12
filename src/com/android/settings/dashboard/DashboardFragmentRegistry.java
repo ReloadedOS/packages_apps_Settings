ReloadedOS/packages_apps_Settings

commit bf8c9009d848eeeaaf1daebaf5e40e4d102152c7

Settings: Add ReloadedXtras to dashboard
Signed-off-by: madmax7896 <madmax78968@gmail.com>
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -48,6 +48,8 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import org.reloadedos.xtras.ReloadedXtras;
+
 import java.util.Map;
 
 /**
@@ -125,6 +127,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_BATTERY_SAVER_SETTINGS);
         PARENT_TO_CATEGORY_KEY_MAP.put(SmartBatterySettings.class.getName(),
                 CategoryKey.CATEGORY_SMART_BATTERY_SETTINGS);
+        PARENT_TO_CATEGORY_KEY_MAP.put(ReloadedXtras.class.getName(),
+                CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 

.gitignore
@@ -1,2 +1,3 @@
 .idea/
 *.iml
+ReloadedXtras/*

Android.bp
@@ -67,7 +67,16 @@ android_library {
         "btadva_settings_java_defaults",
     ],
 
-    srcs: ["src/**/*.java", "src/**/*.kt"],
+    srcs: ["src/**/*.java", "src/**/*.kt",
+           "ReloadedXtras/src/**/*.java",
+    ],
+
+    aaptflags: ["--extra-packages org.reloadedos.xtras"],
+
+    resource_dirs: [
+        "res",
+        "ReloadedXtras/res",
+    ],
 
     static_libs: [
         "androidx-constraintlayout_constraintlayout",
@@ -100,6 +109,7 @@ android_library {
         "lottie",
         "WifiTrackerLib",
         "SettingsLibActivityEmbedding",
+        "kotlin-stdlib",
     ],
 
     libs: [

AndroidManifest.xml
@@ -4537,6 +4537,19 @@
             </intent-filter>
         </activity>
 
+        <!-- ReloadedXtras -->
+        <activity android:name=".Settings$ReloadedXtrasActivity"
+            android:label="@string/reloaded_xtras_dash_title"
+            android:icon="@drawable/ic_settings_reloaded"
+            android:exported="true"
+            android:parentActivityName="com.android.settings">
+            <intent-filter>
+                <action android:name="com.android.settings.action.SETTINGS" />
+            </intent-filter>
+            <meta-data android:name="com.android.settings.FRAGMENT_CLASS"
+                android:value="org.reloadedos.xtras.ReloadedXtras" />
+        </activity>
+
         <!-- This is the longest AndroidManifest.xml ever. -->
     </application>
 </manifest>

ReloadedXtras
@@ -1,0 +1,1 @@
+../ReloadedXtras

proguard.flags
@@ -17,6 +17,8 @@
     *;
 }
 
+-keep class org.reloadedos.xtras.**
+
 # We want to keep methods in Activity that could be used in the XML attribute onClick.
 -keepclassmembers class * extends android.app.Activity {
     public void *(android.view.View);

res/drawable/ic_settings_reloaded.xml
@@ -1,0 +1,11 @@
+<vector xmlns:android="http://schemas.android.com/apk/res/android"
+    android:width="24dp"
+    android:height="24dp"
+    android:viewportWidth="24"
+    android:viewportHeight="24"
+    android:tint="?attr/colorControlNormal">
+  <path
+      android:fillColor="#FF000000"
+      android:pathData="M6.5,8.292C10.155,8.297 13.81,8.294 17.464,8.295C17.489,11.391 17.468,14.488 17.476,17.585C17.506,17.894 17.306,18.174 17.067,18.349C16.623,18.526 16.134,18.462 15.668,18.484C15.668,19.585 15.674,20.685 15.662,21.785C15.717,22.372 15.185,22.853 14.649,22.971C14.269,22.995 13.853,23.036 13.52,22.81C13.14,22.618 12.903,22.206 12.897,21.785C12.864,20.687 12.886,19.587 12.885,18.488C12.318,18.469 11.752,18.471 11.186,18.474C11.146,18.685 11.088,18.895 11.086,19.111C11.062,20.116 11.107,21.121 11.061,22.126C10.756,23.119 9.294,23.283 8.64,22.553C8.406,22.335 8.347,22 8.329,21.694C8.325,20.622 8.327,19.55 8.324,18.477C7.881,18.48 7.414,18.533 6.998,18.34C6.67,18.203 6.48,17.833 6.495,17.488C6.492,14.424 6.469,11.358 6.5,8.292ZM19.257,8.396C19.649,8.335 20.109,8.284 20.452,8.53C20.832,8.77 21.173,9.184 21.122,9.658C21.121,11.855 21.124,14.052 21.12,16.25C21.169,16.737 20.815,17.17 20.407,17.394C19.564,17.895 18.392,17.213 18.345,16.256C18.341,14.088 18.345,11.919 18.343,9.752C18.321,9.162 18.756,8.657 19.257,8.396ZM3.876,8.371C4.243,8.343 4.654,8.3 4.964,8.541C5.371,8.756 5.62,9.194 5.61,9.653C5.611,11.887 5.617,14.12 5.606,16.353C5.585,17.315 4.355,17.867 3.573,17.386C3.179,17.156 2.843,16.727 2.875,16.253C2.874,14.053 2.873,11.854 2.874,9.656C2.851,9.06 3.33,8.543 3.876,8.371ZM13.556,11.799C13.686,12.343 13.71,12.911 13.65,13.468C13.416,14.203 12.705,14.657 11.977,14.794C11.956,14.46 11.961,14.117 11.839,13.799C11.437,14.211 11,14.588 10.625,15.026C10.48,15.188 10.696,15.33 10.795,15.439C11.161,15.782 11.487,16.176 11.915,16.445C11.946,16.157 11.89,15.855 11.975,15.578C12.71,15.396 13.474,15.117 13.934,14.477C14.625,13.616 14.53,12.366 14.113,11.404C13.925,11.532 13.721,11.641 13.556,11.799ZM11.876,9.263C11.865,9.56 11.86,9.858 11.834,10.154C11.259,10.193 10.716,10.453 10.246,10.776C9.764,11.105 9.479,11.635 9.283,12.169C9.227,12.597 9.238,13.033 9.277,13.462C9.357,13.756 9.499,14.028 9.599,14.315C9.787,14.178 9.979,14.042 10.131,13.864C10,13.308 9.812,12.687 10.107,12.147C10.374,11.479 11.059,11.044 11.746,10.939C11.873,11.237 11.859,11.572 11.956,11.88C12.341,11.457 12.78,11.083 13.141,10.637C13.226,10.557 13.123,10.456 13.071,10.394C12.697,9.991 12.297,9.614 11.876,9.263ZM7.452,1.669C7.291,1.32 7.644,0.878 8.022,1.025C8.624,1.407 9.036,2.025 9.614,2.436C10.107,2.221 10.624,2.042 11.161,1.981C11.683,1.954 12.208,1.956 12.731,1.977C13.326,2.026 13.892,2.23 14.441,2.453C14.96,1.995 15.366,1.407 15.953,1.025C16.336,0.901 16.675,1.323 16.515,1.682C16.224,2.104 15.807,2.414 15.471,2.797C15.333,3.056 15.592,3.243 15.754,3.406C16.862,4.424 17.513,5.924 17.484,7.431C13.816,7.442 10.148,7.439 6.481,7.435C6.517,5.753 7.35,4.126 8.613,3.028C8.613,2.996 8.612,2.932 8.612,2.901C8.245,2.472 7.776,2.137 7.452,1.669ZM13.844,4.731C13.844,5.019 13.843,5.306 13.845,5.594C14.077,5.593 14.457,5.588 14.689,5.586C14.69,5.3 14.689,5.014 14.69,4.726C14.457,4.725 14.077,4.729 13.844,4.731ZM9.275,4.735C9.279,5.018 9.279,5.302 9.278,5.585C9.534,5.586 10.136,5.576 10.135,5.575C10.134,5.574 10.13,4.723 10.125,4.723C9.842,4.726 9.558,4.724 9.275,4.735Z"
+      android:fillType="nonZero"/>
+</vector>

res/values/reloaded_strings.xml
@@ -8,4 +8,8 @@
     <!-- ReloadedOS versioning -->
     <string name="reloaded_version_title">ReloadedOS version</string>
 
+    <!-- Reloaded Xtras -->
+    <string name="reloaded_xtras_dash_title">Reloaded Xtras</string>
+    <string name="reloaded_xtras_summary">Additional customizations</string>
+
 </resources>

commit 7a68174e9b9bcafd0349ff9f29e6bff9d3620808

Settings: Do not map ReloadedXtras to developer settings
We don't want preferences injected into developer
settings fragment in here. One such example is
"System Tracing".

Fixes: bf8c9009d848eeeaaf1daebaf5e40e4d102152c7 ("Add ReloadedXtras to dashboard")

src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -48,8 +48,6 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
-import org.reloadedos.xtras.ReloadedXtras;
-
 import java.util.Map;
 
 /**
@@ -127,8 +125,6 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_BATTERY_SAVER_SETTINGS);
         PARENT_TO_CATEGORY_KEY_MAP.put(SmartBatterySettings.class.getName(),
                 CategoryKey.CATEGORY_SMART_BATTERY_SETTINGS);
-        PARENT_TO_CATEGORY_KEY_MAP.put(ReloadedXtras.class.getName(),
-                CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 